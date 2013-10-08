225ecc79d2191c02629fdb72d3aa20e11aba65ca
chanthou <chanthou@web-essentials.asia>
2013-10-08 08:14:46 +0700

    Add new File

---
+++ Configuration/TSConfig_/default/nav/main_menu/setup.ts
@@ -0,0 +1,14 @@
+#######################
+### Main Menu Block ###
+#######################
+
+lib.textmenu = HMENU
+lib.textmenu {
+	1 = TMENU
+	1.NO.allWrap = <li>|</li>
+
+	1.ACT = 1
+	1.ACT.wrapItemAndSub = <li class="active">|</li>
+
+	1.wrap = <ul class="nav">|</ul>
+}
---
+++ Configuration/TSConfig_/default/setup.ts
@@ -0,0 +1,5 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mybox/Configuration/TSConfig/default/nav/setup.ts">
+
+page = PAGE
+
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mybox/Configuration/TSConfig/default/page/setup.ts">
